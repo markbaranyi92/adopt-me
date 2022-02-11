911d62e87e58e64b6a2f930dd11010615267a4f7
markbaranyi92 <mark.baranyi92@gmail.com>
2022-02-11 18:30:07 +0100

    eslint-react, select list

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
-
-    
+    const [animal, setAnimal] = useState("");
 
     return (
         <div className="search-params">
@@ -13,6 +13,24 @@ const SearchParams = () => {
                     Location
                     <input id="location" onChange={(e) => setLocation(e.target.value)} value={location.toUpperCase()}
                     placeholder="Location" />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}
+                    >
+                        <option />
+                        {
+                            ANIMALS.map(animal => (
+                                <option value={animal} key={animal}>
+                                    {animal}
+                                </option>
+                            ))
+                        }
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
