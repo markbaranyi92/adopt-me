d012f9913d6f09629588c7f96ab38569718ba00a
markbaranyi92 <mark.baranyi92@gmail.com>
2022-02-11 17:26:31 +0100

    remove comments

--- src/app.js
+++ src/app.js
@@ -6,28 +6,6 @@ import ReactDOM from "react-dom";
 
 import Pet from "./Pet";
 
-/* const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    ...[1, 2, 3, 4].map((i) => React.createElement("h2", {}, i)), //spread operator  ...
-    React.createElement(Pet, {
-      name: "Luna?????",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
-}; */
-
 const App = () => {
   return (
     <div>
