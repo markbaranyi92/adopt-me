c9f662ae425b2e405dc3f361f669fa14f086cf7f
markbaranyi92 <mark.baranyi92@gmail.com>
2022-02-13 22:44:45 +0100

    usebreedlist, results

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom";
 //eslint - npm run lint
 //parcel - run/bundle - npm run dev
 
-import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Birdog" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
       <SearchParams />
     </div>
   );
